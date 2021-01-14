8dd20a8c5388c747575374f0d08c51e30360fa3d
Tad Fisher <tadfisher@gmail.com>
2021-01-13 16:01:51 -0800

    plexpass: update

--- pkgs/plex-plexpass/raw.nix
+++ pkgs/plex-plexpass/raw.nix
@@ -9,9 +9,9 @@
 
 plexRaw.overrideAttrs (attrs: rec {
   pname = attrs.pname + "-plexpass";
-  version = "1.21.1.3830";
+  version = "1.21.1.3876";
   src = fetchurl {
-    url = "https://downloads.plex.tv/plex-media-server-new/1.21.1.3830-6c22540d5/debian/plexmediaserver_1.21.1.3830-6c22540d5_amd64.deb";
-    sha256 = "0hh9bkbkha0c91kizv8w6wm5l64z303jz7r961w8bpzv93sk3b0m";
+    url = "https://downloads.plex.tv/plex-media-server-new/1.21.1.3876-3c3adfcb4/debian/plexmediaserver_1.21.1.3876-3c3adfcb4_arm64.deb";
+    sha256 = "1xpsmk5l0f0blqp5ba9n1w0npsk692p07hp4ipkq7yz3mfag50p0";
   };
 })
