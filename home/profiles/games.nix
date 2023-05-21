9e4ee0e8ce870c24aa6ef8dbc60c539f3dfac27c
Tad Fisher <tadfisher@gmail.com>
2023-05-21 15:32:47 -0700

    openmw speedup

--- home/profiles/games.nix
+++ home/profiles/games.nix
@@ -1,18 +1,25 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    gnome.gnome-chess
-    enyo-doom
-    gzdoom
-    openmw
-    portmod
-    protontricks
-    retroarch
-    steam-run
-    stockfish
-    vkquake
-    yquake2
-    yquake2-all-games
-  ];
+  home = {
+    packages = with pkgs; [
+      gnome.gnome-chess
+      enyo-doom
+      gzdoom
+      openmw
+      portmod
+      protontricks
+      retroarch
+      steam-run
+      stockfish
+      vkquake
+      yquake2
+      yquake2-all-games
+    ];
+
+    sessionVariables = {
+      # https://gitlab.com/OpenMW/openmw/-/issues/6185
+      OSG_VERTEX_BUFFER_HINT = "VERTEX_BUFFER_OBJECT";
+    };
+  };
 }
